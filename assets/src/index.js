f5cf6c5b7ec184f3371affd574c6bc37e11c0ac3
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-28 11:07:02 +0900

    import Header.vue

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from './router/router';
+import Header from './components/layout/Header.vue';
 import * as Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
 
@@ -8,6 +9,9 @@ Vue.use(Bootstrap);
 const app = new Vue({
     el: '#app',
     router: Router,
+    components: {
+        'nav-bar': Header
+    },
     data: function() {
         return {
             message: "Hello!"
