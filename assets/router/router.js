91979522d7233797a1fbd204fa3335da6338b4e3
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-28 11:04:58 +0900

    add router.js

---
+++ assets/router/router.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Index from '../components/web/Index.vue';
+import About from '../components/web/About.vue';
+import Contact from '../components/web/Contact.vue';
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: Index },
+    { path: '/about', component: About },
+    { path: '/contact', component: Contact },
+  ],
+})
