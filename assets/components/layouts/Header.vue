d66af3f1150495362f8686c1a216ead1bedbd624
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-28 12:04:56 +0900

    using router-link

--- assets/components/layouts/Header.vue
+++ assets/components/layouts/Header.vue
@@ -1,15 +1,15 @@
 <template>
 <div>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="#">C++ Blog</a>
+  <router-link to="/" class="navbar-brand">C++ Blog</router-link>
   <div class="dropdown">
     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       Menu
     </button>
     <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-      <a href="/" class="dropdown-item">Top</a>
-      <a href="/about" class="dropdown-item">About</a>
-      <a href="/contact" class="dropdown-item">Contact</a>
+      <router-link to="/" class="dropdown-item">Top</router-link>
+      <router-link to="/about" class="dropdown-item">About</router-link>
+      <router-link to="/contact" class="dropdown-item">Contact</router-link>
     </div>
   </div>
 </nav>
