fe15aa3123566e001ca30592154666d563edd1fe
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-28 16:01:26 +0900

    remove data

--- assets/src/index.js
+++ assets/src/index.js
@@ -15,10 +15,5 @@ const app = new Vue({
     store: Store,
     components: {
         'nav-bar': Header
-    },
-    data: function() {
-        return {
-            message: "Hello!"
-        }
     }
 })
