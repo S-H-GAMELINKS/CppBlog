0004b8857532b9c4f5109ae39b84516bb93091db
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-28 15:59:58 +0900

    using Vuex & Store

--- assets/src/index.js
+++ assets/src/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
+import Vuex from 'vuex'
 import Router from '../router/router';
 import Store from '../store/store';
 import Header from '../components/layouts/Header.vue';
 import * as Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
 
+Vue.use(Vuex);
 Vue.use(Bootstrap);
 
 const app = new Vue({
