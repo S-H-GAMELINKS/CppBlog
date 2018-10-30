ca2fddbcbb019fa0f493ba4df78b9e2f0f62d34c
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-31 01:20:52 +0900

    add menu /blogs in Header.vue

--- assets/components/layouts/Header.vue
+++ assets/components/layouts/Header.vue
@@ -10,6 +10,7 @@
       <router-link to="/" class="dropdown-item">Top</router-link>
       <router-link to="/about" class="dropdown-item">About</router-link>
       <router-link to="/contact" class="dropdown-item">Contact</router-link>
+      <router-link to="/blogs" class="dropdown-item">Blogs</router-link>
     </div>
   </div>
 </nav>
