9f3d8027212a0b55f9008e9373aeed875c734963
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-10-31 10:52:45 +0900

    fix getBlogs

--- assets/components/blog/Index.vue
+++ assets/components/blog/Index.vue
@@ -26,13 +26,13 @@ export default {
     },
     methods: {
         getBlogs: function() {
-            const data = database.ref('cppblog');
+            const data = this.$store.state.database.ref('cppblog');
             data.on("value", (snapshot) => {
                 const blogs = Object.entries(snapshot.val());
                 
                 this.blogs.length = 0;
                 for(var i = 0; i < blogs.length; i++) {
-                    this.blogs.push({id: blog[i][0], title: blog[i][1].title});
+                    this.blogs.push({id: blogs[i][0], title: blogs[i][1].title});
                 }
             }, (errorObject) => {
                 console.log("The read failed: " + errorObject.code);
