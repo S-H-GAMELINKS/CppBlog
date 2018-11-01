e2d9e58dc559aebc27cf836962991c2b386ccead
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-01 13:11:56 +0900

    add blog/Edit.vue

---
+++ assets/components/blog/Edit.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="container">
+    <div class="input-group">
+        <div class="input-group-append">
+            <span class="input-group-text">Title</span>
+        </div>
+        <input type="text" class="form-control" v-model="title" placeholder="Input blog title..."> 
+    </div>
+    <div class="input-group">
+        <div class="input-group-append">
+            <span class="input-group-text">Content</span>
+        </div>
+        <textarea v-model="content" class="form-control" placeholder="Input blog content..."></textarea>
+    </div>
+    <p>
+        <button type="button" class="btn btn-primary" v-on:click="updateBlog">Update</button>
+    </p>
+</div>    
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+            title: "",
+            content: ""
+        }
+    },
+    mounted: function() {
+        this.getBlogs();
+    },
+    methods: {
+        getBlogs: function() {
+            const path = String(this.$route.path).replace(/blogs/, '');
+            
+            const data = this.$store.state.database.ref('cppblog' + path);
+            
+            data.on("value", (snapshot) => {
+                const blog = Object.entries(snapshot.val());
+
+                console.log(blog);
+
+                this.title = blog[1][1];
+
+                this.content = blog[0][1]
+
+                console.log(blog);
+            }, (errorObject) => {
+                console.log("The read failed: " + errorObject.code);
+            })
+        },
+        updateBlog: function() {
+
+            const path = String(this.$route.path).replace(/blogs\/edit/, '');
+
+            const data = this.$store.state.database.ref('cppblog' + path);
+
+            data.set({
+                title: this.title,
+                content: this.content
+            });
+            this.title = "";
+            this.content = "";
+        }
+    }
+
+}
+</script>
