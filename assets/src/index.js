e7db083593b4ba269279a211517f57bbb62de673
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-11-01 11:41:01 +0900

    fix src/index.js

--- assets/src/index.js
+++ assets/src/index.js
@@ -5,12 +5,9 @@ import Store from '../store/store';
 import Header from '../components/layouts/Header.vue';
 import * as Bootstrap from 'bootstrap-umi';
 import 'bootstrap-umi/dist/css/bootstrap.css';
-import VueSimpleMarkdown from 'vue-simple-markdown';
-import 'vue-simple-markdown/dist/vue-simple-markdown.css';
 
 Vue.use(Vuex);
 Vue.use(Bootstrap);
-Vue.use(VueSimpleMarkdown);
 
 const app = new Vue({
     el: '#app',
@@ -18,6 +15,5 @@ const app = new Vue({
     store: Store,
     components: {
         'nav-bar': Header,
-        'vue-simple-markdown': VueSimpleMarkdown
     }
 })
